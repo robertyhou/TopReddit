c9c56922fa6d9ef9d50d21ca7d800ca8ac2f9ce7
samuelfu <sfu2000@gmail.com>
2019-01-03 13:52:14 -0800

    Update Parser

--- Parser.py
+++ Parser.py
@@ -2,3 +2,10 @@
 https://www.reddit.com/r/all/hot.json?limit=100
 
 '''
+import urllib.request, json
+url = 'https://www.reddit.com/r/all/hot.json?limit=100'
+jsonurl = urllib.request.urlopen(url)
+text = json.loads(jsonurl.read())
+children = text['data']['children']
+for child in children:
+    print(child['data']['subreddit'])
