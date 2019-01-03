e9c0840adb44ef62d523f5573378c031cafc6f42
Robert Hou <hour@berkeley.edu>
2019-01-03 15:00:42 -0800

    Templates

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from config import config
 bootstrap = Bootstrap()
 moment = Moment()
 
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -17,8 +16,5 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
