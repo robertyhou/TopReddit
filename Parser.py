9c842d23210975249bb7f4084779532400bb2069
Robert Hou <hour@berkeley.edu>
2019-01-03 15:48:18 -0800

    Parser now counts subreddits

--- Parser.py
+++ Parser.py
@@ -1,8 +1,29 @@
 from Authorizer import Authorizer
-url = 'https://oauth.reddit.com/r/all/hot.json?limit=100'
+import pandas as pd
+url = 'https://oauth.reddit.com/r/popular/hot.json?limit=1000'
 
 authorizer = Authorizer(url)
 text = authorizer.getJSON()
 children = text['data']['children']
+subCount = {}
 for child in children:
-    print(child['data']['subreddit'])
+    #print(child['data']['subreddit'])
+    subReddit = child['data']['subreddit']
+    if subReddit in subCount:
+        subCount[subReddit] += 1
+    else:
+        subCount[subReddit] = 1
+print(subCount)
+
+subReddits = []
+counts = []
+
+for sub in subCount:
+    subReddits.append(sub)
+    counts.append(subCount[sub])
+
+print(subReddits)
+print(counts)
+
+df = pd.DataFrame({'sub':subReddits, 'val':counts})
+ax = df.plot.bar(x='lab', y='val', rot=0)
