2c2899fbb6b2601aeea60d6a4bb4cfe6f41a75de
Robert Hou <hour@berkeley.edu>
2019-01-03 15:09:14 -0800

    removed vestiges; flask and templates now working

--- Parser.py
+++ Parser.py
@@ -3,9 +3,15 @@ https://www.reddit.com/r/all/hot.json?limit=100
 
 '''
 import urllib.request, json
-url = 'https://www.reddit.com/r/all/hot.json?limit=100'
+url = 'https://www.reddit.com/r/all/hot.json?limit=500'
 jsonurl = urllib.request.urlopen(url)
 text = json.loads(jsonurl.read())
 children = text['data']['children']
+countDict = {}
 for child in children:
-    print(child['data']['subreddit'])
+    subreddit = child['data']['subreddit']
+    if subreddit not in countDict:
+        countDict[subreddit] = 1
+    else:
+        countDict[subreddit] += 1
+print(countDict)
