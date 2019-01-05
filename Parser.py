b0ea4c872556b4bf8cc462f2af1a4aca16f78402
samuelfu <sfu2000@gmail.com>
2019-01-04 22:58:25 -0800

    Cleaned up Parser.py code

--- Parser.py
+++ Parser.py
@@ -4,65 +4,53 @@ import numpy as np
 
 class Parser:
     def __init__(self, limit=100, subreddit='all', t='day'):
-        authorizer = []
-        authorizer.append(Authorizer(limit, subreddit=subreddit, t=t))
+        authorizer = Authorizer(limit, subreddit=subreddit, t=t)
+        request_JSON = authorizer.getJSON()
+        subCount = {}
+        for text in request_JSON:
+            children = text['data']['children']
+            for child in children:
+                subReddit = child['data']['subreddit']
+                if subReddit in subCount:
+                    subCount[subReddit] += 1
+                else:
+                    subCount[subReddit] = 1
 
+        sorted_dictionary = sorted((value, key) for (key, value) in subCount.items())[::-1]
 
-        self.mode_dict = {}
-        self.count_dict = {}
+        self.subReddits = []
+        self.counts = []
+        for dict in sorted_dictionary:
+            self.subReddits.append(dict[1])
+            self.counts.append(dict[0])
 
-        for mode in authorizer:
-            request_JSON = mode.getJSON()
-            type = mode.get_subreddit()+mode.get_time()
-            subCount = {}
-            for text in request_JSON:
-                children = text['data']['children']
-                for child in children:
-                    subReddit = child['data']['subreddit']
-                    if subReddit in subCount:
-                        subCount[subReddit] += 1
-                    else:
-                        subCount[subReddit] = 1
-
-            sorted_dictionary = sorted((value, key) for (key, value) in subCount.items())[::-1]
-
-            subReddits = []
-            counts = []
-            for dict in sorted_dictionary:
-                subReddits.append(dict[1])
-                counts.append(dict[0])
-
-            self.mode_dict[type] = subReddits
-            self.count_dict[type] = counts
 
     def plot(self):
-        for key, value in self.mode_dict.items():
-            objects = self.mode_dict[key][:10]
-            y_pos = np.arange(len(objects))
-            performance = self.count_dict[key][:10]
+        objects = self.subReddits[:10]
+        y_pos = np.arange(len(objects))
+        performance = self.counts[:10]
 
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.tight_layout()
+        plt.xticks(y_pos, objects, fontsize=8)
+        ax = plt.gca()
+        ax.set_aspect(aspect=0.2)
+        pad = 5
+        for tick in ax.xaxis.get_major_ticks()[0:]:
+            tick.set_pad(pad)
+            if pad < 20:
+                pad += 10
+            elif pad > 20:
+                pad = 5
 
-            plt.bar(y_pos, performance, align='center', alpha=0.5)
-            plt.tight_layout()
-            plt.xticks(y_pos, objects, fontsize=8)
-            ax = plt.gca()
-            ax.set_aspect(aspect=0.2)
-            pad = 5
-            for tick in ax.xaxis.get_major_ticks()[0:]:
-                tick.set_pad(pad)
-                if pad < 20:
-                    pad += 10
-                elif pad > 20:
-                    pad = 5
-
-            plt.ylabel('Count')
-            plt.title('Top Subreddits of the Day')
-            plt.savefig('app/static/graph.png')
-            plt.clf()
-            plt.cla()
-            plt.close()
+        plt.ylabel('Count')
+        plt.title('Top Subreddits of the Day')
+        plt.savefig('app/static/graph.png')
+        plt.clf()
+        plt.cla()
+        plt.close()
 
 
 if __name__ == '__main__':
-    parser = Parser()
+    parser = Parser(5)
     parser.plot()
