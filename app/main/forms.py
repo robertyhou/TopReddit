1686679872b77781fca54d3046678904fa5d1f16
Robert Hou <hour@berkeley.edu>
2019-01-04 20:59:31 -0800

    more options for form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class QueryForm(FlaskForm):
-    numPosts = StringField('Number of posts to Query', validators=[DataRequired()])
+    numPosts = IntegerField('Number of Posts to Query', validators=[DataRequired()])
+    keywords = SelectField('Frequency Type', choices=[(0, 'Subreddits'), (1, 'Keywords')]) #should remove popular english words (the, a etc.)
+    time = SelectField('Within ', choices=[(0, '24hr'), (1, 'Week'), (2, 'Month'), \
+                                                   (3, 'Year'), (4, 'All time')])
+    forum = SelectField('From ', choices=[(0, '/r/all'), (1, '/r/popular')])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -6,5 +6,7 @@ from .forms import QueryForm
 @main.route('/')
 def index():
     form = QueryForm()
+    if form.validate_on_submit():
+        print(5)
     temp()
     return render_template('index.html', form=form)
