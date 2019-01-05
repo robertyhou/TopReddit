df9e4a616ee4018b8d595b092c3e7fbf6358d7d3
Robert Hou <hour@berkeley.edu>
2019-01-04 21:55:59 -0800

    Form is for the most part done

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 
 class QueryForm(FlaskForm):
-    numPosts = IntegerField('Number of Posts to Query', validators=[DataRequired()])
-    keywords = SelectField('Frequency Type', choices=[(0, 'Subreddits'), (1, 'Keywords')]) #should remove popular english words (the, a etc.)
-    time = SelectField('Within ', choices=[(0, '24hr'), (1, 'Week'), (2, 'Month'), \
-                                                   (3, 'Year'), (4, 'All time')])
-    forum = SelectField('From ', choices=[(0, '/r/all'), (1, '/r/popular')])
+    numPosts = IntegerField('Number of Posts to Query', validators=[DataRequired(), NumberRange(min=1, max=1000, message='Must query between 1 and 1000 posts')])
+    keywords = SelectField('Frequency Type', choices=[('0', 'Subreddits'), ('1', 'Keywords')]) #should remove popular english words (the, a etc.)
+    time = SelectField('Within ', choices=[('0', '24hr'), ('1', 'Week'), ('2', 'Month'), \
+                                                   ('3', 'Year'), ('3', 'All time')])
+    form = SelectField('From ', choices=[('1', '/r/all'), ('2', '/r/popular')])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from . import main
 from Parser import temp
 from .forms import QueryForm
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
     form = QueryForm()
     if form.validate_on_submit():
-        print(5)
+        return redirect(url_for('main.index'))
     temp()
     return render_template('index.html', form=form)
