493c947dac67a4311d137e10a728a75001b62830
Robert Hou <robertyhou@gmail.com>
2019-01-09 20:01:18 -0800

    Authorizer comments

--- Authorizer.py
+++ Authorizer.py
@@ -31,18 +31,21 @@ class Authorizer():
     """Obtains JSON data for under 100 posts"""
     def simple_request(self, limit=100):
         self.url += str(limit)
+
+        # client id, secret key (tied to application made with ssf2130)
         client_auth = requests.auth.HTTPBasicAuth('15umy6vmCIHQ7A', 'uQgAoXAeOV6r6S3AKc9V8i7fFQU')
+
         post_data = {"grant_type": "password", "username": "ssf2130", "password": "github"}
         headers = {"User-Agent": "TopReddit/0.1 by ssf2130"}
         response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data,
-                                 headers=headers)
-        token = response.json()['access_token']
+                                 headers=headers) #return response that gives access to reddit api
+        token = response.json()['access_token'] #from the response, retrive the token which gives access
         self.headers = {"Authorization": "bearer " + token,
-                   "User-Agent": "TopReddit/0.1 by ssf2130"}
+                   "User-Agent": "TopReddit/0.1 by ssf2130"} #user-agent is purpose
 
-        request_JSON = requests.get(self.url, headers=self.headers).json()
+        request_JSON = requests.get(self.url, headers=self.headers).json() #json with reddit data used in parser
         self.JSON_page_list.append(request_JSON)
-        self.after = request_JSON['data']['after']
+        self.after = request_JSON['data']['after'] #bookmark so that next request gives next 100
 
     """Obtains JSON data for over 100 posts"""
     def multiple_requests(self, limit=200):
